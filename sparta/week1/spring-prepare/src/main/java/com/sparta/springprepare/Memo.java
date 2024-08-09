386a86fb5b24a515049288d992cfe566bc3dcaff
kang-sumin <min001117@gmail.com>
2024-08-09 17:51:53 +0900

    [FEAT] @AllArgsConstructor & @NoArgsConstructor

--- sparta/week1/spring-prepare/src/main/java/com/sparta/springprepare/Memo.java
+++ sparta/week1/spring-prepare/src/main/java/com/sparta/springprepare/Memo.java
@@ -1,11 +1,23 @@
 package com.sparta.springprepare;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Memo {
     private String username;
     private String contents;
+}
+
+class Main{
+    public static void main(String[] args) {
+        Memo memo = new Memo("Robbert", "내용");
+        memo.setUsername("Kate");
+        System.out.println(memo.getUsername());
+    }
 }
