ed824151a2d03bb7941b55b9b9a8c60192c45ffb
kang-sumin <min001117@gmail.com>
2024-08-12 14:06:38 +0900

    [UPDATE] 동적페이지 반환

--- sparta/week1/spring-mvc/src/main/java/com/sparta/springmvc/html/HtmlController.java
+++ sparta/week1/spring-mvc/src/main/java/com/sparta/springmvc/html/HtmlController.java
@@ -1,10 +1,13 @@
 package com.sparta.springmvc.html;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HtmlController {
+
+    private static long visitCount = 0;
 
     @GetMapping("/static-hello")
     public String hello(){
@@ -15,5 +18,18 @@ public class HtmlController {
     @GetMapping("/html/redirect")
     public String htmlStatic(){
         return "redirect:/hello.html";
+    }
+
+    //thymeleaf 엔진을 이용한 정적 페이지 반환
+    @GetMapping("/html/templates")
+    public String htmlTemplates(){
+        return "hello";
+    }
+
+    @GetMapping("/html/dynamic")
+    public String htmlDynamic(Model model){
+        visitCount++;
+        model.addAttribute("visits",visitCount);
+        return "hello-visit";
     }
 }
