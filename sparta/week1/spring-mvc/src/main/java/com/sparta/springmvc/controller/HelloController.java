58919e750c7f419158a50fe71dd4f1ecad7b09b6
kang-sumin <min001117@gmail.com>
2024-08-12 13:45:45 +0900

    [UPDATE] 정적인 페이지 Controller로 반환

--- sparta/week1/spring-mvc/src/main/java/com/sparta/springmvc/controller/HelloController.java
+++ sparta/week1/spring-mvc/src/main/java/com/sparta/springmvc/controller/HelloController.java
@@ -6,35 +6,33 @@ import org.springframework.web.bind.annotation.*;
 @Controller
 @RequestMapping("/api")
 public class HelloController {
-
     @GetMapping("/hello")
     @ResponseBody
-    public String hello(){
-        return "Hello World!!";
+    public String hello() {
+        return "Hello World!";
     }
 
     @GetMapping("/get")
     @ResponseBody
-    public String get(){
-        return "Get Method 요청";
+    public String get() {
+        return "GET Method 요청";
     }
 
     @PostMapping("/post")
     @ResponseBody
-    public String post(){
-        return "Post Method 요청";
+    public String post() {
+        return "POST Method 요청";
     }
 
     @PutMapping("/put")
     @ResponseBody
-    public String put(){
-        return "Put Method 요청";
+    public String put() {
+        return "PUT Method 요청";
     }
 
     @DeleteMapping("/delete")
     @ResponseBody
-    public String delete(){
-        return "Delete Method 요청";
+    public String delete() {
+        return "DELETE Method 요청";
     }
-
 }
