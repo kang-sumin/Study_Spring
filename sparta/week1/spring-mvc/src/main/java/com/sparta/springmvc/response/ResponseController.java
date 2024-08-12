bd7c47e44c3d362ef59988148b52ffe907956165
kang-sumin <min001117@gmail.com>
2024-08-12 15:29:55 +0900

    [COMMENT]  ResponseController 주ì 수정

--- sparta/week1/spring-mvc/src/main/java/com/sparta/springmvc/response/ResponseController.java
+++ sparta/week1/spring-mvc/src/main/java/com/sparta/springmvc/response/ResponseController.java
@@ -5,31 +5,26 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.util.Stack;
-
 @Controller
 @RequestMapping("/response")
 public class ResponseController {
-
-    /**
-     * Content-Type : text/html
-     * Response Body
-     * {"name":"Robbie", "age":95}
-     */
+    // [Response header]
+    //   Content-Type: text/html
+    // [Response body]
+    //   {"name":"Robbie","age":95}
     @GetMapping("/json/string")
     @ResponseBody
     public String helloStringJson() {
         return "{\"name\":\"Robbie\",\"age\":95}";
     }
 
-    /**
-     * Content-Type : application/json
-     * Response Body
-     * {"name":"Robbie", "age":95}
-     */
+    // [Response header]
+    //   Content-Type: application/json
+    // [Response body]
+    //   {"name":"Robbie","age":95}
     @GetMapping("/json/class")
     @ResponseBody
-    public Star helloClassJson(){
-        return new Star("Robbie",95);
+    public Star helloClassJson() {
+        return new Star("Robbie", 95);
     }
 }
