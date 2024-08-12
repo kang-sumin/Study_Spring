ff760185f64f8b63f261f67bfc44e65bb71d21b6
kang-sumin <min001117@gmail.com>
2024-08-12 11:22:45 +0900

    [UPDATE] created project and HelloController class

---
+++ sparta/week1/spring-mvc/src/main/java/com/sparta/springmvc/controller/HelloController.java
@@ -0,0 +1,39 @@
+package com.sparta.springmvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/api/hello")
+    @ResponseBody
+    public String hello(){
+        return "Hello World!!";
+    }
+
+    @GetMapping("/api/get")
+    @ResponseBody
+    public String get(){
+        return "Get Method 요청";
+    }
+
+    @PostMapping("/api/post")
+    @ResponseBody
+    public String post(){
+        return "Post Method 요청";
+    }
+
+    @PutMapping("/api/put")
+    @ResponseBody
+    public String put(){
+        return "Put Method 요청";
+    }
+
+    @DeleteMapping("/api/delete")
+    @ResponseBody
+    public String delete(){
+        return "Delete Method 요청";
+    }
+
+}
