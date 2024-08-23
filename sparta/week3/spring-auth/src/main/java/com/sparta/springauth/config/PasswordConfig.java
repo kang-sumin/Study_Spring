daef34ed37233a64d3fb3ba3f31eed343a9afb50
kang-sumin <min001117@gmail.com>
2024-08-23 17:20:21 +0900

    [UPDATE] Bean 수동 등록 : passwordEncoderTest

---
+++ sparta/week3/spring-auth/src/main/java/com/sparta/springauth/config/PasswordConfig.java
@@ -0,0 +1,17 @@
+package com.sparta.springauth.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+// Bean 수동 등록
+@Configuration
+public class PasswordConfig { // passwordConfig
+
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
