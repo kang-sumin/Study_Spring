1bdbe8b4981981884e1d1123e6fc4bc08698d133
kang-sumin <min001117@gmail.com>
2024-08-23 17:02:08 +0900

    [UPDATE] Query Methods 사용해서 SQL 자동 완성

--- sparta/week1/memo/src/main/java/com/sparta/memo/repository/MemoRepository.java
+++ sparta/week1/memo/src/main/java/com/sparta/memo/repository/MemoRepository.java
@@ -3,6 +3,12 @@ package com.sparta.memo.repository;
 import com.sparta.memo.entity.Memo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MemoRepository extends JpaRepository<Memo, Long> {
+import java.util.List;
 
+public interface MemoRepository extends JpaRepository<Memo, Long> {
+    // 수정 시간을 내림차순으로 가져옴
+    List<Memo> findAllByOrderByModifiedAtDesc();
+
+    // 해당 username을 가진 사람이 작성한 모든 메모 내용 가져옴
+    List<Memo> findAllByUsername(String username);
 }
