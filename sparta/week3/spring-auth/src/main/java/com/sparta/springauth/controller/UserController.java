eb3d44fd1649d260aca7d306f43824ed4ce8189d
kang-sumin <min001117@gmail.com>
2024-08-27 22:40:19 +0900

    [FIX] signup redirect path

--- sparta/week3/spring-auth/src/main/java/com/sparta/springauth/controller/UserController.java
+++ sparta/week3/spring-auth/src/main/java/com/sparta/springauth/controller/UserController.java
@@ -32,6 +32,6 @@ public class UserController {
     public String signup(SignupRequestDto requestDto) {
         userService.signup(requestDto);
 
-        return "redirect:/user/login-page";
+        return "redirect:/api/user/login-page";
     }
 }
