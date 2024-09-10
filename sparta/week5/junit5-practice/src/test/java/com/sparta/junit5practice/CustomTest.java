43435fec03ee27185177169cec512d4d07800e9c
kang-sumin <min001117@gmail.com>
2024-09-10 13:51:06 +0900

    [UPDATE] Order로 test 순서 지정

--- sparta/week5/junit5-practice/src/test/java/com/sparta/junit5practice/CustomTest.java
+++ sparta/week5/junit5-practice/src/test/java/com/sparta/junit5practice/CustomTest.java
@@ -1,8 +1,6 @@
 package com.sparta.junit5practice;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Nested;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 public class CustomTest {
 
@@ -14,13 +12,24 @@ public class CustomTest {
 
     @Nested
     @DisplayName("주제 별로 테스트를 그룹지어서 파악하기 좋습니다.")
+    @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
     class Test1 {
+
+        @Order(1)
+        @Test
+        @DisplayName("Test1 클래스")
+        void test() {
+            System.out.println("\nTest1 클래스");
+        }
+
+        @Order(3)
         @Test
         @DisplayName("Test1 - test1()")
         void test1() {
             System.out.println("Test1.test1");
         }
 
+        @Order(2)
         @Test
         @DisplayName("Test1 - test2()")
         void test2() {
