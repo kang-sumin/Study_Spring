18cef12a8ccddc6879a26ca0d2c97746e71ad1a4
kang-sumin <min001117@gmail.com>
2024-09-16 11:03:40 +0900

    [UPDATE] OneToOneTest 단방향 테스트

---
+++ sparta/week4/jpa-advance/src/test/java/com/sparta/jpaadvance/relation/OneToOneTest.java
@@ -0,0 +1,44 @@
+package com.sparta.jpaadvance.relation;
+
+import com.sparta.jpaadvance.entity.Food;
+import com.sparta.jpaadvance.entity.User;
+import com.sparta.jpaadvance.repository.FoodRepository;
+import com.sparta.jpaadvance.repository.UserRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+@Transactional
+@SpringBootTest
+public class OneToOneTest {
+
+    @Autowired
+    FoodRepository foodRepository;
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Test
+    @Rollback(value = false) // 테스트에서 @Transactional에 의해 자동으로 rollback 되어 확인 불가하기 때문에 false 설정해줌
+    @DisplayName("1대1 단방향 테스트")
+    void test1(){
+        // given
+        User user = new User();
+        user.setName("Robbie");
+
+        // 외래 키의 주인인 Food Entity에 user 필드에 user 객체를 추가
+        Food food = new Food();
+        food.setName("후라이드 치킨");
+        food.setPrice(15000);
+        food.setUser(user); // 외래 키(연관관계) 설정
+
+        // when-then
+        userRepository.save(user);
+        foodRepository.save(food);
+
+    }
+
+}
