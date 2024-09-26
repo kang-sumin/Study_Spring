30bbd2d139bf643d84a039c081754e958657a6b4
kang-sumin <min001117@gmail.com>
2024-09-26 20:31:01 +0900

    [INIT] Redis Test Setting file & init project file

---
+++ sparta/docker test/docker-test/src/test/java/com/sparta/dockertest/DockerTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.dockertest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
